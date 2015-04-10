7a3198cb4257c3da112e07ec045db51cee69b1f5
Ronald Phan <phanronald@gmail.com>
2015-04-09 20:56:51 -0700

    fixed the directive. minified code and updated code

--- js/directives/burnItOffDirective.js
+++ js/directives/burnItOffDirective.js
@@ -24,7 +24,7 @@
 			var calories = burnItOffVM.selectedFood.value;
 			var metabolicEquivalentTask = burnItOffVM.selectedExerciseType.value;
 			var correctWeight = parseInt(burnItOffVM.selectedWeight, 10);
-			burnItOffVM.minutesToBurnOffFood = Math.round(calories / (metabolicEquivalentTask * 3.5 * (burnItOffVM.correctWeight / 2.2) / 200) * 10) / 10 + " minutes of " +
+			burnItOffVM.minutesToBurnOffFood = Math.round(calories / (metabolicEquivalentTask * 3.5 * (correctWeight / 2.2) / 200) * 10) / 10 + " minutes of " +
 				burnItOffVM.selectedExerciseType.name + " will burn off an " + burnItOffVM.selectedFood.name;
 		};
 	};
